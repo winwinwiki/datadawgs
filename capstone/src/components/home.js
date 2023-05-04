f6294d1e086ad108b03b7a28862456f4476df577
Alicia Chen <anchovychen@Alicias-MacBook-Pro-4.local>
2023-05-03 17:27:22 -0700

    add tableau

--- capstone/src/components/home.js
+++ capstone/src/components/home.js
@@ -4,14 +4,18 @@ export default function home(props) {
 
     return (
         <div>
-            <h1>
-                Home !
+            <h1 className='title'>
+                Try It Out!
             </h1>
-            <p>The NewImpact Wiki is a free online directory of resources in your impact area. 
+            <p className='description'>The NewImpact Wiki is a free online directory of resources in your impact area. 
                 The unique dataset provided by NewImpact allows you to explore through various modes and filters!
             </p>
-            <p>
-                Familiar with Kibana? Visit our Kibana dashboard <a href="https://wiki.newimpact.care/login?next=%2F#?_g=()" target="_blank">here</a>!
+            <p className='description'>
+                Familiar with Kibana? Visit our Kibana dashboard <a className="kibana" href="https://wiki.newimpact.care/s/datadawgs/app/kibana#/dashboard/53a42230-acbf-11ed-8ffd-45d02d2b23c5?_g=()" target="_blank">here</a>!
+                <p className='smalltext'>
+                    User: DD Shared <br></br>
+                    Password: Sh@reWiki2023!
+                </p>
             </p>
             <Map></Map>
         </div>
